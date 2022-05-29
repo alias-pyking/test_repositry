09efd17881f619bf09b128c05a0695084f4dccb5
alias-pyking <shubhamkumar7051@gmail.com>
2022-05-29 20:15:01 +0530

    add manage.java file

---
+++ manage.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class manage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("Your entered number is " + a);
+        System.out.println("Hello World");
+    }
+}
